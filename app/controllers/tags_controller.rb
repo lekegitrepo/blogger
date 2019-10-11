35bbf77619830de5791e28dda4a5d3b934497b8d
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-11 12:56:19 -0600

    tags view list

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,13 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Article.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Article '#{@tag.article}' deleted!"
+
+    redirect_to tags_path
+  end
 end
